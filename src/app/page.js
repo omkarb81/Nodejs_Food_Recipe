9d493122f1dddb33773320ca746dcfca73c9ee26
Omkar Borate <117004362+omkarb81@users.noreply.github.com>
2024-08-06 00:21:11 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
    <div className="flex justify-center items-center min-h-screen flex-col">
-    <h1 className="text-8xl font-extrabold ">Welcome To recipe app </h1>
-    <Link href={'recipe-list'}> <button className="p-3 mt-10 text-2xl rounded-md text-white bg-black ">Explore Recipe</button> </Link>
+    <h1 className=" lg:text-8xl md:text-6xl text-2xl font-extrabold">Welcome To recipe app </h1>
+    <Link href={'recipe-list'}> <button className="p-3 mt-10 lg:text-2xl text-xs rounded-md text-white bg-black  ">Explore Recipe</button> </Link>
     </div>
   );
 }
